fdbb21091c65884ed5990f5abdedf3b324779bbd
sulaimondawood <sulaimondawod@gmail.com>
2025-11-30 17:17:09 +0100

    added cors configuration

--- src/main/java/com/dawood/sprnt/common/config/SecurityConfig.java
+++ src/main/java/com/dawood/sprnt/common/config/SecurityConfig.java
@@ -1,10 +1,17 @@
 package com.dawood.sprnt.common.config;
 
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.dawood.sprnt.common.security.JwtFilter;
 
@@ -20,13 +27,32 @@ public class SecurityConfig {
 
     return httpSecurity
         .csrf(csrf -> csrf.disable())
-        .cors(cors -> cors.disable())
+        .cors(cors -> cors.configurationSource(null))
         .authorizeHttpRequests(authRequest -> authRequest
             .requestMatchers("/auth/**").authenticated()
             .anyRequest().permitAll())
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
         .build();
+
+  }
+
+  @Bean
+  public BCryptPasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  public CorsConfigurationSource corsConfigurationSource(CorsConfiguration config) {
+
+    config.setAllowCredentials(true);
+    config.setAllowedHeaders(List.of("*"));
+    config.setAllowedMethods(List.of("*"));
+    config.setAllowedOrigins(List.of("http://localhost:3000"));
+
+    UrlBasedCorsConfigurationSource cors = new UrlBasedCorsConfigurationSource();
+    cors.registerCorsConfiguration("/**", config);
+
+    return cors;
 
   }
 
