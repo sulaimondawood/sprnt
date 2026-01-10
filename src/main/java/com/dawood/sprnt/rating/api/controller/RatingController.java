3225c0e00d03955b471a832433f87cc7702f0334
sulaimondawood <sulaimondawod@gmail.com>
2026-01-10 18:31:55 +0100

    added controller to add ride ratings

--- src/main/java/com/dawood/sprnt/rating/api/controller/RatingController.java
+++ src/main/java/com/dawood/sprnt/rating/api/controller/RatingController.java
@@ -1,17 +1,30 @@
 package com.dawood.sprnt.rating.api.controller;
 
 import com.dawood.sprnt.common.dto.ApiResponse;
+import com.dawood.sprnt.rating.api.dto.RideRatingRequest;
+import com.dawood.sprnt.rating.service.RatingService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/ratings")
+@RequiredArgsConstructor
 public class RatingController {
 
-    @PostMapping("/driver")
-    public ResponseEntity<ApiResponse<String>> submitDriverRating(){
+    private final RatingService ratingService;
+
+    @PostMapping()
+    public ResponseEntity<ApiResponse<String>> submitDriverRating(@RequestBody @Valid RideRatingRequest request){
+
+        ratingService.submitRating(request);
+
+        return ApiResponse.created("You have successfully rated the ride");
+
 
     }
 
